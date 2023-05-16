761199f7f49be94618912aa8051e5095a28a6f98
maximumjw <123383544+maximumjw@users.noreply.github.com>
2023-05-17 00:54:23 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ canvas.height=800;
 ctx.fillStyle='white';
 ctx.strokeStyle='white';
 ctx.lineWidth=1;
+var mouse={
+    x: undefined,
+    y: undefined
+}
+window.addEventListener('mousemove',
+    function(event){
+    mouse.x=event.x;
+    mouse.y=event.y;
+    console.log(mouse);
+});
 class Particle{
  constructor(effect){
     this.effect=effect;
@@ -12,14 +22,15 @@ class Particle{
     this.y=Math.floor(Math.random()*this.effect.height);
     this.speedX;
     this.speedY;
-    this.speedModifier=Math.floor(Math.random()*1+1);
+    this.speedModifier=Math.floor(Math.random()*2+1);
     this.history=[{x:this.x,y:this.y}];
-    this.maxLength=Math.floor(Math.random()*60+20);
+    this.maxLength=Math.floor(Math.random()*60+20)*2;
     this.angle=0;
     this.newAngle=0;
     this.andgleCorrector=Math.random()*0.5+0.01;
-    this.timer=this.maxLength*2;
-    this.colors=['#DE8B2C','#E8792E','#D15F34','#E8472E','#DE2C2C'];
+    this.timer=this.maxLength;
+    //this.colors=['#4c026b','#730d9e','9622c7','#b44ae0'];
+    this.colors=['#0000FF','#1E90FF','#00BFFF','#ADD8E6'];
     this.color=this.colors[Math.floor(Math.random()*this.colors.length)];
  }
  draw(context){
@@ -39,27 +50,67 @@ class Particle{
         let y=Math.floor(this.y/this.effect.cellSize);
         let index=y*this.effect.cols+x;
         if(this.effect.flowField[index]){
-        this.newAngle=this.effect.flowField[index].colorAngle;
-        if(this.angle>this.newAngle){
-            this.angle-=this.andgleCorrector;
-        }
-        else if(this.angle<this.newAngle){
-            this.angle+=this.andgleCorrector;
-        }
-        else this.angle=this.newAngle;
+            this.newAngle=this.effect.flowField[index].colorAngle;
+            if(this.angle>this.newAngle){
+                this.angle-=this.andgleCorrector;
+            }
+            else if(this.angle<this.newAngle){
+                this.angle+=this.andgleCorrector;
+            }
+            else this.angle=this.newAngle;
         }
 
-        this.speedX=Math.cos(this.angle)*2;
-        this.speedY=Math.sin(this.angle)*2;
+        //this.speedX=Math.cos(this.angle)*0.5;
+        //this.speedY=Math.sin(this.angle)*0.5;
+
+        this.dir=[-1,1];
+        this.speedX=Math.cos(this.angle)*this.dir[Math.floor(Math.random()*2)]*0.5;
+        this.speedY=Math.sin(this.angle)*this.dir[Math.floor(Math.random()*2)]*0.5;
+        
+        /*if(this.effect.flowField[index]&&!this.dir){
+            if(this.effect.flowField[index].dir>=0){
+                this.dir=1;
+            }
+            else if(this.effect.flowField[index].dir<0){
+                this.dir=-1;
+            }
+        }
+
+        if(this.effect.flowField[index]){
+            if(this.dir>=0){
+                this.speedX=Math.cos(this.angle);
+                this.speedY=Math.sin(this.angle);
+            }
+            else if(this.dir<0){
+                this.speedX=Math.cos(this.angle)*(-1);
+                this.speedY=Math.sin(this.angle)*(-1);
+            }
+        }*/
+
+
         this.x+=this.speedX*this.speedModifier;
         this.y+=this.speedY*this.speedModifier;
-    
-        this.history.push({x:this.x,y:this.y});
-        if(this.history.length>this.maxLength){
-            this.history.shift();
+       if(mouse.x-this.x<50&&mouse.x-this.x>-50&&mouse.y-this.y<50&&mouse.y-this.y>-50){
+          //1: change the speed this.speedModifier=8;
+          this.color='red';
+          this.history.push({x:this.x,y:this.y});
+          if(this.history.length>this.maxLength){
+              this.history.shift();
+          }
         }
+       /* else{
+          //this.speedModifier=1;
+          this.color=this.colors[Math.floor(Math.random()*this.colors.length)];
+          this.history.push({x:this.x,y:this.y});
+            if(this.history.length>this.maxLength){
+            this.history.shift();
+            }
+        }
+        */
+        
     }
     else if(this.history.length>1){
+        //this.dir=0;
         this.history.shift();
     }
     else{
@@ -69,14 +120,15 @@ class Particle{
  reset(){
     let attempts=0;
     let resetSuccess=false;
-    while(attempts<1000&&!resetSuccess){
+
+    while(attempts<100&&!resetSuccess){
         attempts++;
         let textIndex=Math.floor(Math.random()*this.effect.flowField.length);
         if(this.effect.flowField[textIndex].alpha>0){
             this.x=this.effect.flowField[textIndex].x;
             this.y=this.effect.flowField[textIndex].y;
             this.history=[{x:this.x, y:this.y}];
-            this.timer=this.maxLength*0.5;
+            this.timer=this.maxLength*2;
             resetSuccess=true;
         }
     }
@@ -84,7 +136,7 @@ class Particle{
         this.x=Math.random()*this.effect.width;
         this.y=Math.random()*this.effect.height;
         this.history=[{x:this.x, y:this.y}];
-        this.timer=this.maxLength*0.5;
+        this.timer=this.maxLength*2;
     }
  }
 }
@@ -95,7 +147,7 @@ class Effect{
         this.width=this.canvas.width;
         this.height=this.canvas.height;
         this.particles=[];
-        this.numberOfParticles=8000;
+        this.numberOfParticles=10000;
         this.cellSize=5;
         this.rows;
         this.cols;
@@ -104,10 +156,10 @@ class Effect{
         this.init();
         
         window.addEventListener('keydown',e=>{
-        if(e.key==='d')this.debug=!this.debug;
+            if(e.key=='d')this.debug=!this.debug;
         });
         window.addEventListener('resize',e=>{
-         //   this.resize(e.target.innerWidth,e.target.innerHeight);
+            this.resize(e.target.innerWidth,e.target.innerHeight);
         })
     }   
     drawText(){
@@ -129,9 +181,19 @@ class Effect{
         gradient3.addColorStop(0.4, 'rgb(200,255,0)');
         gradient3.addColorStop(0.6, 'rgb(0,0,255)');
         gradient3.addColorStop(0.8, 'rgb(0,0,0)');
+        const gradient4=this.context.createRadialGradient(this.width*0.5,this.height*0.5,10,this.width*0.5,this.height*0.5,this.width);
+        gradient4.addColorStop(0.2, 'rgb(128,0,128)');
+        gradient4.addColorStop(0.4, 'rgb(218,112,214)');
+        gradient4.addColorStop(0.6, 'rgb(255,0,255)');
+        gradient4.addColorStop(0.8, 'rgb(128,0,128)');
+        const gradient5=this.context.createRadialGradient(this.width*0.7,this.height*0.7,10,this.width*0.5,this.height*0.5,this.width);
+        gradient5.addColorStop(0.1, 'rgb(0,0,225)');
+        gradient5.addColorStop(0.4, 'rgb(30,144,255)');
+        gradient5.addColorStop(0.6, 'rgb(0,191,255)');
+        gradient5.addColorStop(0.8, 'rgb(173,216,230)');
         //27:35 색깔 실험 
-        this.context.fillStyle=gradient1;
-        this.context.fillText('FIRE',this.width*0.5,this.height*0.5,this.width*0.8);
+        this.context.fillStyle=gradient5;
+        this.context.fillText('SASA',this.width*0.5,this.height*0.5,this.width);
 
     }   
     init(){
@@ -158,7 +220,8 @@ class Effect{
                     x:x,
                     y:y,
                     alpha:alpha,
-                    colorAngle:colorAngle
+                    colorAngle:colorAngle,
+                    dir:Math.random()*(-1)+0.5
                 });
             }
         }   
